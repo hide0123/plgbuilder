2cba35572946b4c581981fd289329cbb1fc9a863
HIDE <48782538+HIDE810@users.noreply.github.com>
2021-01-16 22:47:20 +0900

    Use $PORT

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def result():
     return render_template('result.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
